ee6bb7bfd4c649b92b2af09dfa467bd3cc0fa4bd
yuv_windows <yuv_aubee@hotmail.com>
2025-11-13 21:21:12 +0100

    minor fixes

--- first_tutorials/scripts/module0.py
+++ first_tutorials/scripts/module0.py
@@ -21,7 +21,7 @@ def push0(ball_id, step):
             p.applyExternalForce(
                 ball_id,                # bodyUniqueId: the ID of the object you want to apply the force to
                 -1,                     # linkIndex: -1 means apply force to the base link (for single-link objects)
-                forceObj=[5, 5, 0],     # The force vector (Fx, Fy, Fz) in Newtons
+                forceObj=[1, 5, 0],     # The force vector (Fx, Fy, Fz) in Newtons
                 posObj=[0, 0, 0],       # The point of application of the force relative to the link’s frame
                 flags=p.WORLD_FRAME     # Frame in which the force is defined. Can also use p.LINK_FRAME. Keep World frame for now
             )
